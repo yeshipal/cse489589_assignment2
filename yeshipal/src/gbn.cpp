d7de2bdea177b59f3f128080d5ff0d5e96f65873
Yeshi Paljor <92138464+yeshipal@users.noreply.github.com>
2022-05-11 02:57:00 -0400

    edit

--- yeshipal/src/gbn.cpp
+++ yeshipal/src/gbn.cpp
@@ -33,7 +33,7 @@ void A_output(struct msg message)
     starttimer(AHOST, RTT);
     base++;
     }
-    else(base < getwinsize())
+    if(base < getwinsize())
     {
         lastpkt = packets.at(seq);
         tolayer3(AHOST, lastpkt);
